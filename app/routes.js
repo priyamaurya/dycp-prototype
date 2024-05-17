c53ef8806925641fd9821364842811844c2986dc
zali-goaco <z.ali@goaco.com>
2024-05-17 13:47:24 +0100

    v4

--- app/routes.js
+++ app/routes.js
@@ -9,33 +9,31 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here
 
 // Route index page
+  
+router.get('/one-login', function (req, res) {
+  req.session.proto_version = req.query.v;
+  res.render('one-login/index', {})
+})
+
+
+router.get('/one-login/enter-code', function (req, res) {
+  res.render('one-login/enter-code', {proto_version: req.session.proto_version})
+});
+
+router.post('/supplier-information-v1/org-number', function (req, res) {
+  res.redirect('org-location');
+});
+
+
+router.post('/supplier-information-v1/contracting-authority', function (req, res) {
+  if(req.body.contractingAuthority == 'yes')
+    res.redirect('/supplier-information-v1/contracting-authority-type')
+  else
+    res.redirect('/supplier-information-v1/org-number')
+});
 
   
-  router.get('/one-login', function (req, res) {
-    req.session.proto_version = req.query.v;
-    res.render('one-login/index', {})
-  })
-  
-  
-  router.get('/one-login/enter-code', function (req, res) {
-    res.render('one-login/enter-code', {proto_version: req.session.proto_version})
-  });
-  
-  router.post('/supplier-information-v1/org-number', function (req, res) {
-    res.redirect('org-location');
-  });
-  
-  
-  
-  router.post('/supplier-information-v1/contracting-authority', function (req, res) {
-    if(req.body.contractingAuthority == 'yes')
-      res.redirect('/supplier-information-v1/contracting-authority-type')
-    else
-      res.redirect('/supplier-information-v1/org-number')
-  })
-
-  
-
+  /*
 router.post('/supplier-information-v3/org-type', function (req, res) {
     var orgType = req.body.orgType;
     req.session.orgType = orgType;
@@ -46,7 +44,7 @@ router.post('/supplier-information-v3/org-type', function (req, res) {
     res.redirect('/supplier-information-v3/has-org-id');
   });
   
-  /*
+
   router.post('/supplier-information-v3/has-ch-number', function (req, res) {
     if(req.body.chno == 'no')
       res.redirect('/supplier-information-v3/org-id');
@@ -92,17 +90,229 @@ router.post('/supplier-information-v3/org-type', function (req, res) {
   });    */
 
 
-  router.post('/supplier-information-v4/org-type', function (req, res) {
-    var orgType = req.body.orgType;
-    req.session.orgType = orgType;
+router.get('/supplier-information-v4/start', function (req, res) {
+  // hack to simulate manage users page
+  req.session.allowManageUsers = 0;
+  res.render('supplier-information-v4/start');
+});
 
-    res.redirect('/supplier-information-v4/has-ch-number');
-  });
 
-  router.post('/supplier-information-v4/has-ch-number', function (req, res) {
-    if(req.body.chno == 'no')
-      res.redirect('/supplier-information-v4/org-id');
-    else
+router.post('/supplier-information-v4/org-type', function (req, res) {
+  var orgType = req.body.orgType;
+  req.session.orgType = orgType;
+
+  res.redirect('/supplier-information-v4/has-ch-number');
+});
+
+router.post('/supplier-information-v4/has-ch-number', function (req, res) {
+  if(req.body.chno == 'no'){
+    res.redirect('/supplier-information-v4/org-id');
+  } else {
+    if(req.body.chnoInput == "12345678"){
+      res.redirect('/supplier-information-v4/org-exists');
+    } else {
       res.redirect('/supplier-information-v4/org-name');
-  });
+    }
+  }
+});
+
+router.post('/supplier-information-v4/org-name', function (req, res) {
+  if(req.body.orgName.toLowerCase() == "defra"){
+    res.redirect('/supplier-information-v4/org-exists');
+  } else {
+    res.redirect('/supplier-information-v4/org-email');
+  }
+});
+
+
+router.get('/supplier-information-v4/org-address-uk', function (req, res) {
+  res.render('supplier-information-v4/org-address-uk', {orgType: req.session.orgType});
+});
   
+router.post('/supplier-information-v4/org-address-uk', function (req, res) {
+  if (req.session.orgType  == 'buyer'){
+    res.redirect('/supplier-information-v4/buyer-type');
+  } else {
+    res.redirect('/supplier-information-v4/org-overview'); 
+  }
+}); 
+
+
+router.get('/supplier-information-v4/manage-users', function (req, res) {
+
+  console.log(req.session.allowManageUsers);
+  if (req.session.allowManageUsers == 0) {
+    res.render('supplier-information-v4/manage-users/cannot-manage-add-org');
+  } else if (req.session.allowManageUsers == 2) {
+    res.render('supplier-information-v4/manage-users/manage-users',{prompt: "userRequest"});
+  } else {
+    res.render('supplier-information-v4/manage-users/manage-users',{notification: req.query.n});
+  }
+  req.session.allowManageUsers++
+});
+
+router.post('/supplier-information-v4/manage-users/remove-user', function (req, res) {
+  res.redirect('/supplier-information-v4/manage-users?n=userRemoved');
+});
+
+router.post('/supplier-information-v4/manage-users/add-user', function (req, res) {
+  res.redirect('/supplier-information-v4/manage-users?n=userAdded');
+});
+
+router.post('/supplier-information-v4/manage-users/update-user-admin', function (req, res) {
+  res.redirect('/supplier-information-v4/manage-users?n=userUpdatedCurrent');
+});
+
+router.post('/supplier-information-v4/manage-users/update-user', function (req, res) {
+  res.redirect('/supplier-information-v4/manage-users?n=userUpdated');
+});
+
+
+/* connected persons */
+
+router.post('/supplier-information-v4/connected-persons/connected-question', function (req, res) {
+  res.redirect('/supplier-information-v4/connected-persons/companies-question');
+});
+
+router.post('/supplier-information-v4/connected-persons/companies-question', function (req, res) {
+  res.redirect('/supplier-information-v4/connected-persons/person-question');
+});
+
+router.post('/supplier-information-v4/connected-persons/person-question', function (req, res) {
+  res.redirect('/supplier-information-v4/connected-persons/persons');
+});
+
+router.post('/supplier-information-v4/connected-persons/persons', function (req, res) {
+
+  let connectedPersons = req.session.data.connectedPersons;
+
+  if (connectedPersons == 'A person with significant control over the supplier') {
+    res.redirect('/supplier-information-v4/connected-persons/psc-individual');
+
+  } else if (connectedPersons == 'A director or shadow director of the supplier') {
+    res.redirect('/supplier-information-v4/connected-persons/director-individual');
+
+  } else if (connectedPersons == 'A director or shadow director of the supplier (org)') {
+    res.redirect('/supplier-information-v4/connected-persons/director-individual-ni');
+
+  } else if (connectedPersons == 'A relevant legal entity (registrable)') {
+    res.redirect('/supplier-information-v4/connected-persons/gov-organisation');
+
+  } else if (connectedPersons == 'PSC (Person of significant control) organisation or Public Authority') {
+    res.redirect('/supplier-information-v4/connected-persons/psc-individual-ni');
+
+  } else if (connectedPersons == 'A parent undertaking or a subsidiary undertaking of the supplier') {
+    res.redirect('/supplier-information-v4/connected-persons/parent-sub');
+
+  } else if (connectedPersons == 'A predecessor company of the supplier') {
+    res.redirect('/supplier-information-v4/connected-persons/predecessor');
+
+  } else if (connectedPersons == 'An organisation with the right to exercise control') {
+    res.redirect('/supplier-information-v4/connected-persons/right');
+
+  } else if (connectedPersons == 'Right to exercise control') {
+    res.redirect('/supplier-information-v4/connected-persons/right');
+
+  } else {
+    res.redirect('/supplier-information-v4/connected-persons/journey-page');
+  }
+
+});
+
+
+router.post('/supplier-information-v4/connected-persons/gov-organisation', function (req, res) {
+  res.redirect('gov-reg-address-type');
+});
+
+router.post('/supplier-information-v4/connected-persons/director-individual-ni', function (req, res) {
+  res.redirect('director-reg-address-type-ni');
+});
+
+router.post('/supplier-information-v4/connected-persons/parent-sub', function (req, res) {
+  res.redirect('parent-reg-address-type');
+});
+
+router.post('/supplier-information-v4/connected-persons/predecessor', function (req, res) {
+  res.redirect('pred-reg-address-type');
+});
+
+router.post('/supplier-information-v4/connected-persons/right', function (req, res) {
+
+  let personQuestion = req.session.data.personQuestion;
+
+  if (personQuestion == "organisation") {
+    res.redirect('right-reg-address-type');
+  }
+  else {
+    res.redirect('right-residency');
+  }
+});
+
+router.post('/supplier-information-v4/connected-persons/psc-individual', function (req, res) {
+  res.redirect('address-type');
+});
+
+router.post('/supplier-information-v4/connected-persons/director-individual', function (req, res) {
+  res.redirect('director-residency');
+});
+
+router.post('/supplier-information-v4/connected-persons/gov-reg-address-type', function (req, res) {
+  let addressTypeGovReg = req.session.data.addressTypeGovReg;
+
+  if (addressTypeGovReg == "No") {
+    res.redirect('gov-reg-address');
+  } else {
+    res.redirect('find-reg-address-gov');
+  }
+});
+
+
+router.post('/supplier-information-v4/connected-persons/director-reg-address-type-ni', function (req, res) {
+
+  let addressTypeRegDirNi = req.session.data.addressTypeRegDirNi;
+
+  if (addressTypeRegDirNi == "No") {
+    res.redirect('dir-reg-address-ni');
+  } else {
+    res.redirect('find-reg-address-dir-ni');
+  }
+});
+
+router.post('/supplier-information-v4/connected-persons/gov-reg-address', function (req, res) {
+  res.redirect('gov-service-same');
+});
+
+router.post('/supplier-information-v4/connected-persons/gov-reg-address-uk', function (req, res) {
+  res.redirect('gov-service-same');
+});
+
+router.post('/supplier-information-v4/connected-persons/gov-service-same', function (req, res) {
+
+  let serviceSame = req.session.data.serviceSame;
+
+  if (serviceSame == "Yes") {
+    res.redirect('gov-law-register');
+  } else {
+    res.redirect('gov-address-type');
+  }
+})
+
+
+router.post('/supplier-information-v4/connected-persons/gov-law-register', function (req, res) {
+  res.redirect('nature-of-control-gov');
+})
+
+
+router.post('/supplier-information-v4/connected-persons/nature-of-control-gov', function (req, res) {
+  res.redirect('date-registered-gov');
+})
+
+
+router.post('/supplier-information-v4/connected-persons/date-registered-gov', function (req, res) {
+  res.redirect('gov-register');
+})
+
+
+router.post('/supplier-information-v4/connected-persons/gov-register', function (req, res) {
+  res.redirect('check-answers-connected-person');
+})
