d0803d37b03ac632daccd3abe5e680b82f1440ca
zali-goaco <z.ali@goaco.com>
2024-07-10 11:28:49 +0100

    routes - wip

--- app/views/supplier-information-v5/connected-person/routes.js
+++ app/views/supplier-information-v5/connected-person/routes.js
@@ -2,47 +2,67 @@ const express = require('express')
 const router = express.Router()
 const path = require('node:path')
 
-// var journey=[
-//     "has-connected-person", 
-//     "is-registered-with-companies-house", 
-//     "type", 
-//     "category" 
-// ];
-
-// for (let i = 0; i < journey.length; i++) {
-//     if(journey[i] == req.url.substring(1)){
-//     console.log(req.body)
-//     res.redirect(journey[i+1])
-//     }
-// }
-
-
   
 var oRoute = {
     "/has-connected-person": "is-registered-with-companies-house",
     "/is-registered-with-companies-house": "type",
     "/type": "category",
-    "/category": "address"
+    "/category": "name",
+    "/name": "address",
+    "/address": "is-same-address",
+    "/is-same-address": "address-2",
+    "/address-2": "legal-formation",
+    "/legal-formation": "companies-house-number",
+    "/companies-house-number": "nature-of-control",
+    "/nature-of-control": "registerable-date",
+    "/registerable-date": "register-name",
+    "/register-name": "check-your-answers",
+    "/check-your-answers": "add-another"
 }
 
 var oViews = {
-    "/random-name": "name.html"
+    "/address-2": "address.html",
+    "/registerable-date": "date.html"
 }
 
-router.get('/random-name', function (req, res) {
-    console.log(path.resolve(__dirname, oViews[req.url]));
-    res.render(path.resolve(__dirname, oViews[req.url]));
+
+router.post('/has-connected-person', function (req, res, next) {
+    if(req.body.hasConnectedPerson == 'No')
+        res.redirect('../supplier-information');
+    else
+        return next();
 });
 
-router.post('/type', function (req, res) {
-    res.redirect("random-name")
+router.post('/type', function (req, res, next) {
+
+    switch(req.body.typeOfConnectedPerson){
+        case 'Individual':
+            console.log(req.body.typeOfConnectedPerson);
+            break;
+        case 'Trustee or trust':
+            console.log(req.body.typeOfConnectedPerson);
+            break;
+        default:
+            return next();
+
+    }
+
+    if(req.body.hasConnectedPerson == 'No')
+        res.redirect('../supplier-information');
+    else
+        return next();
+});
+
+
+router.get('/*', function (req, res, next) {
+    if(oViews[req.url])
+        res.render(path.resolve(__dirname, oViews[req.url]), {page: req.url});
+    else
+        res.render(path.resolve(__dirname, `${req.url.substring(1)}.html`), {page: req.url});
 });
    
 router.post('/*', function (req, res, next) {
-    console.log("target", oRoute[req.url])
     res.redirect(oRoute[req.url]);
-
-    //return next(); 
 });
 
 module.exports = router;
